f894d13e7ae54842d3dbd4d81bac14fb8b2a6f90
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-02-27 21:46:46 +0700

    Add src/component/Heder.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+import React, { Component } from 'react'
 import { AppRegistry } from 'react-native';
-import App from './App';
+// import App from './App';
 
-AppRegistry.registerComponent('instagram', () => App);
+// AppRegistry.registerComponent('instagram', () => App);
+
+import Header from './src/component/Header'
+
+class App extends Component {
+  render() {
+    return(
+      <Header headerText="Instagram"/>
+    )
+  }
+}
+
+AppRegistry.registerComponent('instagram', () => App)
